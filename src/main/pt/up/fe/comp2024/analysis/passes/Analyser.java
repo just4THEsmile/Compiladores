9a2c07fdbd869032d9696bbd0736d6c2b6620fab
JustaName <diogosarmento4@gmail.com>
2024-04-19 18:44:29 +0100

    fixed weird issue

--- src/main/pt/up/fe/comp2024/analysis/passes/Analyser.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/Analyser.java
@@ -62,7 +62,7 @@ public class Analyser extends AnalysisVisitor {
                 isImport=true;
             }
         }
-        if( importName.endsWith(table.getClassName())){
+        if( importName.equals(table.getClassName())){
             addReport(new Report(ReportType.ERROR, Stage.SEMANTIC, NodeUtils.getLine(node), NodeUtils.getColumn(node),
                     "Import name has the same name of class " + importName));
             return null;
@@ -289,13 +289,19 @@ public class Analyser extends AnalysisVisitor {
         String methodSignature = getMethodSignature(methodNode);
 
         // check if method is duplicated
+        Boolean isMethod=false;
         for (String method : table.getMethods()) {
             if (method.equals(methodName)) {
-                int line = methodNode.get("lineStart") != null ? Integer.parseInt(methodNode.get("lineStart")) : -1;
-                int column = methodNode.get("colStart") != null ? Integer.parseInt(methodNode.get("colStart")) : -1;
-                String message = "Method '" + methodName + "' is already declared";
-                addReport(new Report(ReportType.ERROR, Stage.SEMANTIC, line, column, message));
-                return null;
+                if (isMethod) {
+
+
+                    int line = methodNode.get("lineStart") != null ? Integer.parseInt(methodNode.get("lineStart")) : -1;
+                    int column = methodNode.get("colStart") != null ? Integer.parseInt(methodNode.get("colStart")) : -1;
+                    String message = "Method '" + methodName + "' is already declared";
+                    addReport(new Report(ReportType.ERROR, Stage.SEMANTIC, line, column, message));
+                    return null;
+                }
+                isMethod=true;
             }
         }
 
